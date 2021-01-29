456d5357179e8ac4e04102671628b4198d3a1df9
TheKadir <cruznadin01@gmail.com>
2021-01-29 22:48:13 +0300

    start

---
+++ src/yourdata.js
@@ -0,0 +1,168 @@
+// Skills Icons
+import htmlIcon from "./images/html.svg"
+import cssIcon from "./images/css.svg"
+import reactIcon from "./images/react.svg"
+import jsIcon from "./images/javascript.svg"
+import designIcon from "./images/design.svg"
+import codeIcon from "./images/code.svg"
+import NodeIcon from "./images/node-brands.svg";
+import PhpIcon from "./images/php-brands.svg";
+// Social Icon
+import githubIcon from "./images/github.svg"
+import codepenIcon from "./images/codepen.svg"
+import dribbbleIcon from "./images/dribbble.svg"
+import instagramIcon from "./images/instagram.svg"
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "Kadir",
+  headerTagline: [
+    //Line 1 For Header
+    "Web & Mobile",
+    //Line 2 For Header
+    "products, brands,",
+    //Line 3 For Header
+    "and experience",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    "Mobile and Web Developer at Tulpar Yazılım.",
+
+  //Contact Email
+  contactEmail: "contact@thekadir.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      id: 1, //DO NOT CHANGE THIS (Please)😅
+      title: "Alanyaspor", //Project Title - Add Your Project Title Here
+      para:
+        "", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://www.alanyaspor.org.tr/upload/vektors/alanyaspor-logo-png.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://www.alanyaspor.org.tr/",
+    },
+    {
+      id: 2, //DO NOT CHANGE THIS (Please)😅
+      title: "Alanya.bel.tr", //Project Title - Add Your Project Title Here
+      para:
+        "", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://www.alanya.bel.tr/img/www/logo.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://www.alanya.bel.tr/",
+    },
+    {
+      id: 3, //DO NOT CHANGE THIS (Please)😅
+      title: "Tulwork Mobile App", //Project Title - Add Your Project Title Here
+      para:
+        "", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://www.tulwork.com/assets/Logo.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://www.tulwork.com/",
+    },
+    {
+      id: 4, //DO NOT CHANGE THIS (Please)😅
+      title: "Tultrack", //Project Title - Add Your Project Title Here
+      para:
+        "", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://www.tultrack.com/images/logo.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://www.tultrack.com/",
+    },
+    {
+      id: 5, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Five", //Project Title - Add Your Project Title Here
+      para:
+        "", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://www.tulparyazilim.com.tr/img/logo.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://www.tulparyazilim.com.tr/",
+    },
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+    ,{
+        id: 5,
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "Hello! My name is Kadir Barcın. I am a Web Developer and I am very passionate about my work. With 7 years of experience as a professional Web developer, I have acquired the necessary knowledge and skills to succeed.\n",
+  aboutParaTwo:
+    "I enjoy this step of discussion and collaboration, concept and design as much as possible, but I find the greatest satisfaction to see the finished product doing everything it was created for.",
+  aboutParaThree:
+    "",
+  aboutImage:
+    "https://images.unsplash.com/photo-1519713958759-6254243c4a53?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+    {
+      id: 1,
+      img: PhpIcon,
+      para:
+          "PHP & Wordpress",
+    },
+    {
+      id: 2,
+      img: NodeIcon,
+      para:
+        "Node.js, React Native, Express, Electron, Gatsby, Next.js",
+    },
+    {
+      id: 3,
+      img: jsIcon,
+      para:
+        "Java Script",
+    },
+  ],
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+  promotionHeading:  "My experiences",
+  promotionPara:
+    "Mobile Application With React Native and Node js, my Appstore 4 Google Play 6 applications are online and actively used.",
+  // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+  contactSubHeading: "Let's create your next experience together",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { img: githubIcon, url: "https://github.com/CruzNadin" },
+
+  ],
+
+  // End Contact Section ---------------
+}
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
